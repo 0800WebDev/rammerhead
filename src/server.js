657b995969e4b24ab44017db2c1425ac95646afa
0800 <spmspy0800@gmail.com>
2026-03-22 08:00:56 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,1 +1,6 @@
 require('./server/index.js');
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://pgis.onrender.com");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
